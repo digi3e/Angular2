d1e6898b1f4b0b5ba3df4d765a31599038e8b5a5
digi3e@gmail.com <T@blature2>
2020-04-03 14:41:23 -0500

    lab - all from memory

--- src/app/walt/walt.component.ts
+++ src/app/walt/walt.component.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WaltComponent implements OnInit {
 
+  userName: string = 'username'
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  checkUsername() {
+    return !this.userName;
+
+    // if (this.userName == '') {
+    //   return true;
+    // }
+    //
+    // return false;
+  }
+
+  resetUsername() {
+    this.userName = ''
   }
 
 }
